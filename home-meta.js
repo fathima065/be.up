16e3a530f6b1991f0195a93384e937604734ad05
fathima065 <npfathima06@gmail.com>
2026-09-15 22:05:10 +0530

    Polish homepage document metadata

---
+++ home-meta.js
@@ -0,0 +1,7 @@
+(function(){
+'use strict';
+if(window.top!==window.self||window.location.pathname!=='/')return;
+document.title='Be^Up | Business Excellence & Unlimited Performance';
+var d=document.querySelector('meta[name="description"]');
+if(d)d.setAttribute('content','Be^Up helps businesses improve performance across IT, Finance, Marketing, Operations and Sales. Your Growth. Our Solution.');
+})();
